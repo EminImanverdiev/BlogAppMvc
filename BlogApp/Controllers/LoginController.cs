36c988a4ebcd7562eb4e3410fcec7251fb928263
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-29 16:35:30 +0400

    added Authorization and Authentication

---
+++ BlogApp/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlogApp.Controllers
+{
+	public class LoginController : Controller
+	{
+		[AllowAnonymous]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		[AllowAnonymous]
+		public async Task<IActionResult> Index(Writer writer)
+		{
+			AppDbContext context = new AppDbContext();
+			var datavalues = context.Writers.FirstOrDefault(w => w.WriterEmail == writer.WriterEmail && w.WriterPassword == writer.WriterPassword);
+            if (datavalues!=null)
+            {
+				var claims = new List<Claim>
+				{
+					new Claim(ClaimTypes.Name,writer.WriterEmail)
+				};
+				var useridentity=new ClaimsIdentity(claims,"N");
+				ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(useridentity);
+				await HttpContext.SignInAsync(claimsPrincipal);
+				return RedirectToAction("Index", "Writer"); 
+            }
+			else
+			{
+				return View();
+			}
+        }
+	}
+}
+//AppDbContext context = new AppDbContext();
+//var datavalues = context.Writers.FirstOrDefault(w => w.WriterEmail == writer.WriterEmail && w.WriterPassword == writer.WriterPassword);
+//if (datavalues != null)
+//{
+//	HttpContext.Session.SetString("username", writer.WriterEmail);
+//	return RedirectToAction("Index", "Writer");
+//}
+//else
+//{
+//	return View();
+//}
--- BlogApp/Startup.cs
+++ BlogApp/Startup.cs
@@ -2,9 +2,12 @@ using Business.Abstract;
 using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +32,20 @@ namespace BlogApp
         {
             services.AddServices(); 
             services.AddControllersWithViews();
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                             .RequireAuthenticatedUser()
+                             .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+            services.AddMvc();
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,9 +64,9 @@ namespace BlogApp
             app.UseStatusCodePagesWithReExecute("/ErrorPage/Error","?code={0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseAuthentication();
             app.UseRouting();
-
+            app.UseSession();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

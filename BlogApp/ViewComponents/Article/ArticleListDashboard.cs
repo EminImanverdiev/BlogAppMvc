ea7ee45318e7094c45767828e708ddda33297fa0
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-31 22:11:36 +0400

    second commit

---
+++ BlogApp/ViewComponents/Article/ArticleListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.ViewComponents.Article
+{
+    public class ArticleListDashboard:ViewComponent
+
+    {
+        IArticleService _articleService;
+
+        public ArticleListDashboard(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var result = _articleService.GetArticlesWithCategory();
+            return View(result);
+        }
+    }
+}
